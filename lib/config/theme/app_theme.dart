2ff7961f1a1ef41d283b6102eb8928e1c9d73078
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-05-16 14:23:54 -0600

    :sparkles:Agregando Assets

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:app_vida_saludable/config/theme/app_colors.dart';
+
+class AppTheme {
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        primaryColor: AppColors.primary,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: AppColors.primary,
+          primary: AppColors.grey,
+        ),
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(
+            fontFamily: 'MuseoSans',
+            fontWeight: FontWeight.w500,
+          ),
+          bodyText2: TextStyle(
+            fontFamily: 'MuseoSans',
+            fontWeight: FontWeight.w500,
+          ),
+        ).apply(
+          bodyColor: Colors.black,
+          displayColor: Colors.black,
+        ),
+      );
+}
