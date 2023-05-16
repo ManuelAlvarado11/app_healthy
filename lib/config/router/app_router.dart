406a2556d25ac5e2855aef71b7d7dbbe108434c0
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-05-16 15:03:28 -0600

    :art:Maquetacion inicial de LoginScreen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,6 +1,19 @@
 import 'package:go_router/go_router.dart';
+import 'package:app_vida_saludable/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [],
+  initialLocation: '/login',
+  routes: [
+    // LOGIN
+    GoRoute(
+      path: '/login',
+      name: LoginScreen.name,
+      builder: (context, state) {
+        return const LoginScreen();
+      },
+    ),
+
+    // REDIRECT
+    GoRoute(path: '/', redirect: (_, __) => '/login')
+  ],
 );
---
+++ lib/presentation/widgets/widgets.dart
@@ -0,0 +1,3 @@
+export './shared/custom_bottom_navigation.dart';
+export './shared/custom_text_form_field.dart';
+export './shared/custom_text_button.dart';
