afb462902da806e556244648004925a4dc37472b
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-05-18 09:42:44 -0600

    :building_construction: Mover carpeta Utils

--- lib/utils/inputs/inputs.dart
+++
@@ -1,6 +0,0 @@
-export './shared/email.dart';
-export './shared/password.dart';
-export './shared/price.dart';
-export './shared/slug.dart';
-export './shared/stock.dart';
-export './shared/title.dart';
