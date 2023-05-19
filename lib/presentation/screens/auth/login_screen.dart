c49ea3063d1fa71a190ac57e8f162ccfa652ebeb
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-05-19 13:35:00 -0600

    :art: Estilos LoginScreen v2

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:app_vida_saludable/config/theme/app_colors.dart';
 import 'package:app_vida_saludable/presentation/widgets/widgets.dart';
+import 'package:app_vida_saludable/presentation/providers/providers.dart';
+import 'package:app_vida_saludable/config/helpers/mask_formats.dart'
+    as mask_formats;
 
 class LoginScreen extends StatelessWidget {
   static const name = 'login-screen';
@@ -19,31 +24,10 @@ class LoginScreen extends StatelessWidget {
           body: SafeArea(
             child: SizedBox(
               width: size.width,
-              child: Center(
+              child: const Center(
                 child: SingleChildScrollView(
-                  physics: const ClampingScrollPhysics(),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const SizedBox(height: 30),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: SizedBox(
-                          width: 500,
-                          child: Image.asset(
-                            "assets/img/sis_logo_horizontal.png",
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 70),
-                      SizedBox(
-                        height: size.height - 260,
-                        width: double.infinity,
-                        child: const _LoginForm(),
-                      )
-                    ],
-                  ),
+                  physics: ClampingScrollPhysics(),
+                  child: _LoginForm(),
                 ),
               ),
             ),
@@ -52,91 +36,122 @@ class LoginScreen extends StatelessWidget {
   }
 }
 
-class _LoginForm extends StatelessWidget {
+class _LoginForm extends ConsumerWidget {
   const _LoginForm();
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 50),
-      child: Column(
-        children: [
-          // DUI
-          CustomTextFormField(
-            keyboardType: const TextInputType.numberWithOptions(
-                decimal: false, signed: false),
-            icon: const Icon(Icons.person, color: AppColors.grey),
-            label: 'DUI',
-            onChanged: (value) {
-              String dui = value.replaceAll("-", "");
-              return dui;
-            },
-          ),
-          const SizedBox(height: 30),
+  Widget build(BuildContext context, WidgetRef ref) {
+    // PROVIDER
+    final loginForm = ref.watch(loginFormProvider);
 
-          // PASSWORD
-          const CustomTextFormField(
-            icon: Icon(Icons.lock, color: AppColors.grey),
-            label: 'Contraseña',
-            obscureText: true,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        // LOGO
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+            top: 30,
+            bottom: 50,
           ),
-          const SizedBox(height: 10),
-
-          // OLVIDO CONTRASEÑA
-          SizedBox(
-            width: double.infinity,
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                TextButton(
-                  child: const Text(
-                    '¿Olvidaste tu contraseña?',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: AppColors.primary,
-                    ),
-                  ),
-                  onPressed: () => Navigator.pushNamed(context, '/olvidaste'),
-                ),
-              ],
+          child: SizedBox(
+            width: 500,
+            child: Image.asset(
+              "assets/img/sis_logo_horizontal.png",
+              fit: BoxFit.contain,
             ),
           ),
-          const SizedBox(height: 50),
+        ),
 
-          // BUTTON
-          SizedBox(
-            width: double.infinity,
-            height: 60,
-            child: CustomTextButton(
-              text: 'Ingresar',
-              buttonColor: AppColors.primary,
-              onPressed: () {},
-            ),
+        // DUI
+        CustomTextFormField(
+          label: 'DUI',
+          icon: const Icon(Icons.person, color: AppColors.grey),
+          backgroundColor: AppColors.greyLight,
+          labelColor: AppColors.grey,
+          errorMessage: loginForm.isPosted ? loginForm.dui.errorMessage : null,
+          keyboardType: const TextInputType.numberWithOptions(
+            decimal: false,
+            signed: false,
           ),
+          onChanged: (value) {
+            String dui = value.replaceAll("-", "");
+            ref.read(loginFormProvider.notifier).onDuiChange(dui);
+          },
+          listTextInputFormatter: [
+            FilteringTextInputFormatter.digitsOnly,
+            LengthLimitingTextInputFormatter(9),
+            mask_formats.maskFormatterDUI,
+          ],
+        ),
 
-          // REGISTRATE
-          const Spacer(flex: 2),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('¿No tienes cuenta?'),
+        // PASSWORD
+        CustomTextFormField(
+          label: 'Contraseña',
+          icon: const Icon(Icons.lock, color: AppColors.grey),
+          backgroundColor: AppColors.greyLight,
+          labelColor: AppColors.grey,
+          errorMessage:
+              loginForm.isPosted ? loginForm.password.errorMessage : null,
+          obscureText: true,
+          onChanged: (value) {
+            ref.read(loginFormProvider.notifier).onPasswordChange(value);
+          },
+        ),
+        const SizedBox(height: 10),
+
+        // OLVIDO CONTRASEÑA
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
               TextButton(
-                onPressed: () => context.push('/register'),
                 child: const Text(
-                  'Registrate aquí',
+                  '¿Olvidaste tu contraseña?',
                   style: TextStyle(
                     fontSize: 14,
                     color: AppColors.primary,
                   ),
                 ),
-              )
+                onPressed: () => Navigator.pushNamed(context, '/olvidaste'),
+              ),
             ],
           ),
-          const Spacer(flex: 1),
-        ],
-      ),
+        ),
+        const SizedBox(height: 50),
+
+        // BUTTON
+        CustomTextButton(
+          onPressed: () {
+            ref.read(loginFormProvider.notifier).onFormSubmit();
+          },
+          fontsize: 12,
+          txtButton: 'Ingresar',
+        ),
+
+        // REGISTRATE
+        const SizedBox(height: 40),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('¿No tienes cuenta?'),
+            TextButton(
+              onPressed: () => context.push('/register'),
+              child: const Text(
+                'Registrate aquí',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: AppColors.primary,
+                ),
+              ),
+            )
+          ],
+        ),
+      ],
     );
   }
 }
--- lib/presentation/widgets/shared/custom_text_form_field.dart
+++ lib/presentation/widgets/shared/custom_text_form_field.dart
@@ -4,38 +4,33 @@ import 'package:app_vida_saludable/config/theme/app_colors.dart';
 
 class CustomTextFormField extends StatefulWidget {
   final String? label;
+  final Widget? icon;
+  final bool obscureText;
+  final Color? backgroundColor;
+  final Color? labelColor;
+  final bool isSwicth;
+  final double? width;
+
   final String? hint;
   final String? errorMessage;
-  final bool obscureText;
-  final Widget? icon;
   final TextInputType? keyboardType;
   final Function(String)? onChanged;
-  final String? Function(String?)? validator;
-  final EdgeInsets? margin;
-  final double? paddingwidth;
-  final double? fontsize;
-  final Color? color;
-  final Color? colorFill;
-  final bool isSwicth;
   final List<TextInputFormatter>? listTextInputFormatter;
   final TextEditingController? controller;
 
   const CustomTextFormField({
     super.key,
     this.label,
+    this.icon,
+    this.obscureText = false,
+    this.backgroundColor,
+    this.labelColor,
+    this.isSwicth = false,
+    this.width,
     this.hint,
     this.errorMessage,
-    this.obscureText = false,
     this.keyboardType = TextInputType.text,
     this.onChanged,
-    this.validator,
-    this.icon,
-    this.margin,
-    this.paddingwidth,
-    this.fontsize,
-    this.color,
-    this.colorFill,
-    this.isSwicth = false,
     this.listTextInputFormatter,
     this.controller,
   });
@@ -56,66 +51,81 @@ class _CustomTextFormFieldState extends State<CustomTextFormField> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Container(
-        height: 50.0,
-        width: widget.paddingwidth ?? 500,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(50),
-          color: widget.color ?? AppColors.greyLight,
-        ),
-        margin: const EdgeInsets.symmetric(horizontal: 1),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              flex: 5,
-              child: Padding(
-                padding:
-                    EdgeInsets.only(left: 20, right: widget.isSwicth ? 0 : 20),
-                child: Center(
-                  child: TextFormField(
-                    keyboardType: widget.keyboardType ?? TextInputType.text,
-                    inputFormatters: widget.listTextInputFormatter,
-                    controller: widget.controller,
-                    obscureText: isObscure,
-                    onChanged: widget.onChanged,
-                    validator: widget.validator,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      icon: widget.icon,
-                      iconColor: widget.colorFill,
-                      label: widget.label == null
-                          ? null
-                          : Text(widget.label ?? ''),
-                      labelStyle: TextStyle(
-                          color: widget.colorFill, fontSize: widget.fontsize),
-                      hintText: widget.hint ?? '',
-                      suffixIcon: widget.obscureText
-                          ? IconButton(
-                              icon: Icon(
-                                isObscure
-                                    ? Icons.visibility_off
-                                    : Icons.visibility,
-                              ),
-                              onPressed: () {
-                                isObscure = !isObscure;
-                                setState(() {});
-                              },
-                            )
-                          : null,
-                      errorText: widget.errorMessage,
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Container(
+            height: 50.0,
+            width: widget.width ?? 500,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: widget.backgroundColor ?? AppColors.greyLight,
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        left: 20, right: widget.isSwicth ? 0 : 20),
+                    child: Center(
+                      child: TextFormField(
+                        keyboardType: widget.keyboardType ?? TextInputType.text,
+                        inputFormatters: widget.listTextInputFormatter,
+                        controller: widget.controller,
+                        obscureText: isObscure,
+                        onChanged: widget.onChanged,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          icon: widget.icon,
+                          iconColor: widget.labelColor,
+                          hintText: widget.hint ?? '',
+                          label: Text(widget.label ?? ''),
+                          labelStyle: TextStyle(
+                            color: widget.labelColor,
+                            fontSize: 14,
+                          ),
+                          suffixIcon: widget.obscureText
+                              ? IconButton(
+                                  icon: Icon(
+                                    isObscure
+                                        ? Icons.visibility_off
+                                        : Icons.visibility,
+                                  ),
+                                  onPressed: () {
+                                    isObscure = !isObscure;
+                                    setState(() {});
+                                  },
+                                )
+                              : null,
+                        ),
+                      ),
                     ),
                   ),
                 ),
+              ],
+            ),
+          ),
+        ),
+        // Error message
+        Padding(
+          padding: const EdgeInsets.only(left: 40, bottom: 10),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              widget.errorMessage ?? "",
+              style: const TextStyle(
+                fontFamily: 'MuseoSans',
+                color: AppColors.red,
               ),
             ),
-          ],
-        ),
-      ),
+          ),
+        )
+      ],
     );
   }
 }
