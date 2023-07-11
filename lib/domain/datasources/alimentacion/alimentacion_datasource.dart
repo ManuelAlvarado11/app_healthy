5de02299b0ec8f5b7cac14128147bd5a5eb05572
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-07-11 10:41:45 -0600

    :white_check_mark: Add Datasources/Repository

--- lib/domain/datasources/alimentacion/alimentacion_datasource.dart
+++ lib/domain/datasources/alimentacion/alimentacion_datasource.dart
@@ -3,4 +3,5 @@ import 'package:app_vida_saludable/domain/entities/entities.dart';
 abstract class AlimentacionDataSource {
   Future<List<RecomendacionResponse>> getRecomendaciones(int tipo);
   Future<List<ContenidoResponse>> getContenidos(int tipo);
+  Future<List<CategoriaResponse>> getCategorias();
 }
