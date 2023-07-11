13b8183766c5f4f9547d486fb92a88e0e63b66ef
ManuelAlvarado <mantonio.alvarado@salud.gob.sv>
2023-07-11 14:28:33 -0600

    :white_check_mark: Consumir provider para obtener state de la data

---
+++ lib/presentation/screens/vidaSaludable/alimentacion/categorias_alimentacion_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+import 'package:app_vida_saludable/config/theme/app_colors.dart';
+import 'package:app_vida_saludable/presentation/widgets/widgets.dart';
+import 'package:app_vida_saludable/presentation/providers/providers.dart';
+
+class CategoriaAlimentacionScreen extends StatelessWidget {
+  static const routeName = 'categoria-alimentacion-screen';
+
+  const CategoriaAlimentacionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+            child: Material(
+              elevation: 2,
+              color: AppColors.white,
+              borderRadius: BorderRadius.circular(50),
+              child: CupertinoSearchTextField(
+                prefixIcon: const Icon(MdiIcons.magnify),
+                borderRadius: BorderRadius.circular(50),
+                onChanged: (value) => {},
+              ),
+            ),
+          ),
+          _CategoriasList(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CategoriasList extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // PROVIDER
+    final provider = ref.watch(alimentacionProvider);
+
+    return Flexible(
+      child: provider.categorias.isEmpty
+          ? const Center(
+              child: ResultEmptyWidget(
+                mensajeVacio: 'No hay contenido de categoria',
+                urlImagen: "assets/svg/vacio_alimentacion.svg",
+              ),
+            )
+          : RefreshIndicator(
+              onRefresh: () async {
+                ref.read(alimentacionProvider.notifier).getCategorias();
+              },
+              color: AlimentacionColors.secondary,
+              backgroundColor: Colors.white,
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                children: [
+                  ...provider.categorias.toList().map((categoria) {
+                    return CustomCardContentWidget(
+                      contenido: categoria,
+                      tipo: 5,
+                    );
+                  }).toList(),
+                ],
+              ),
+            ),
+    );
+  }
+}
